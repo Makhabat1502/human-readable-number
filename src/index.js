4927efa38452455d99b0d6b0263a442d843c3581
Makhabat Berdibekova <makhabat.berdibekova.kk@gmail.com>
2023-02-07 23:23:49 +0600

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function toReadable (number) {
-  
+    let ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve']
+        teens = ['', '', '', 'thirteen', "forteen", 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+        tenner = ['', 'ten', 'twenty', 'thirty', "fourty", 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    let res = ''
+    let strNumber = String(number)
+    
+    if (number <= 12 ) {
+        return ones[number]
+      } 
+      res = number % 10 != 0 ? ones[number % 10] : ''
+      if (number % 100 <= 12 && number % 100 != 0) {
+        res = ones[number % 100]
+      } else if (number % 100 < 20 && number % 100 != 0) {
+        res = `${teens[number % 100 % 10]}`
+      } else if (number % 100 >= 20 && number % 100 != 0) {
+        res = `${tenner[Math.floor(number % 100/10)]} `+ res
+      }
+      
+      if (number / 100 >= 1) {
+        res = `${ones[Math.floor(number / 100)]} hundred ` + res
+      }
+
+      return res.trim();
 }
+
+
+
+
